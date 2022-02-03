606e01ec22bb42b9cc34d290559d3b8ab8d7c114
Kristen <kkyu@ualberta.ca>
2022-02-03 14:59:46 -0700

    furniture placement, rotation, and picking up

--- pwr/Assets/Scripts/Items/Furniture.cs
+++ pwr/Assets/Scripts/Items/Furniture.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Furniture : Item
 {
     // Start is called before the first frame update
-    public Sprite leftSprite;
-    public Sprite rightSprite;
-    public Sprite frontSprite;
-    public Sprite backSprite;
+
+    public Sprite[] spriteArray;
+    public int currentIndex; 
+    public int maxIndex = 3; 
 
     void Start()
     {
-        
+        currentIndex = 0; 
     }
 
     // Update is called once per frame
