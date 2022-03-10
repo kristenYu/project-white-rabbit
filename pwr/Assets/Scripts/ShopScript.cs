fc930e6a751d00b88a0abc9790c7612dc90d07c1
Samadhi <samadhi@ualberta.ca>
2022-03-10 09:38:55 -0700

    Player movement stopped in shop

--- pwr/Assets/Scripts/ShopScript.cs
+++ pwr/Assets/Scripts/ShopScript.cs
@@ -14,7 +14,7 @@ using TMPro;
      * --- Will need to turn panels on/off
      * 
      * Exit Shop to Main Scene
-     * --- Weird error where player character can move...might want to disable background?? or something??
+     * -- remove HUD in background! should be able to disable it!
      * 
      * Items refresh everyday!
      */
@@ -22,30 +22,54 @@ using TMPro;
 public class ShopScript : MonoBehaviour
 {
     //Exit Shop
-    public Button ExitToMain;
+    public Button ExitToMainButton;
+
+    //Shop item panel button
+    public Button Item1;
+    public Button Item2;
+    public Button Item3;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Remove movement for player
-        //GameObject.Find("Player").GetComponent(PlayerMovement).enabled = false;
-
         //Setting Button to move back to main
-        Button ExitBtn = ExitToMain.GetComponent<Button>();
-        ExitBtn.onClick.AddListener(ButtonClicked);
+        Button ExitBtn = ExitToMainButton.GetComponent<Button>();
+        ExitBtn.onClick.AddListener(ExitButtonClicked);
+
+        //set items
+        Button ItemBtn1 = Item1.GetComponent<Button>();
+        ItemBtn1.onClick.AddListener(ItemPurchased);
+
+       /* Button ItemBtn2 = Item2.GetComponent<Button>();
+        ItemBtn2.onClick.AddListener(ItemPurchased);
+
+        Button ItemBtn3 = Item3.GetComponent<Button>();
+        ItemBtn3.onClick.AddListener(ItemPurchased);*/
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        //purchase item
+
+        //sell item
+
     }
 
-    void ButtonClicked()
+    void ItemPurchased()
     {
-        SceneManager.LoadScene("Main", LoadSceneMode.Single);
-        //Restart movement for player
+        Debug.Log("Pressed!");
+    }
 
+    void ItemSold()
+    {
+
+    }
+    void ExitButtonClicked()
+    {
+        //load main and enable player movement
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
     }
 
 }
